22021550b5d43cd07be7a336e4a252b31b22a6be
hilma <ohadoff2@gmail.com>
2023-01-29 19:24:25 +0200

    end of first day ohad

---
+++ client/src/Components/navBar.jsx
@@ -0,0 +1,22 @@
+function NavBar() {
+  return (
+    <nav id="menu-container">
+      <div class="menu-list">
+        <div>
+          <a href="#">Home</a>
+        </div>
+        <div>
+          <a href="#">Menu</a>
+        </div>
+        <div>
+          <a href="#">About</a>
+        </div>
+        <div>
+          <a href="#">contact</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
