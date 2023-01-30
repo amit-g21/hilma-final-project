306fa929b0b6cc0f220b64dcd92494b8b6f56c21
hilma <amitgonen2468@gmail.com>
2023-01-30 21:46:07 +0200

    last commit day two

--- client/src/Components/Small Components/ToProduct.jsx
+++ client/src/Components/Small Components/ToProduct.jsx
@@ -4,10 +4,10 @@ import "../../clientCss/home.css";
 
 function ToProduct(props) {
   const [product, setProducts] = useState(props.product);
-//   const Navigate = useNavigate();
+  const Navigate = useNavigate();
 
   return (
-    <div className="collections-menu" >
+    <div className="product-menu" onClick={() => Navigate(`${product.product_name}`)}>
       <div className="collection" key={product.id}>
         <h4 className="collection-header">{product.product_name}</h4>
         <img src={""} />
